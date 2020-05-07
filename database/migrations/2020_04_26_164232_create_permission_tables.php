8f4a9ac41ca286d72992ca01bf13e0010a57467a
Jacek Obst <jacekobst1@gmail.com>
2020-05-07 16:29:52 +0000

    Reduce spatie permissions index length

--- database/migrations/2020_04_26_164232_create_permission_tables.php
+++ database/migrations/2020_04_26_164232_create_permission_tables.php
@@ -39,7 +39,7 @@ class CreatePermissionTables extends Migration
 
             $table->string('model_type');
             $table->unsignedBigInteger($columnNames['model_morph_key']);
-            $table->index([$columnNames['model_morph_key'], 'model_type'], 'model_has_permissions_model_id_model_type_index');
+            $table->index([$columnNames['model_morph_key'], 'model_type'], 'm_has_perm_model_id_model_type_index');
 
             $table->foreign('permission_id')
                 ->references('id')
@@ -47,7 +47,7 @@ class CreatePermissionTables extends Migration
                 ->onDelete('cascade');
 
             $table->primary(['permission_id', $columnNames['model_morph_key'], 'model_type'],
-                    'model_has_permissions_permission_model_type_primary');
+                    'm_has_perm_permission_model_type_primary');
         });
 
         Schema::create($tableNames['model_has_roles'], function (Blueprint $table) use ($tableNames, $columnNames) {
