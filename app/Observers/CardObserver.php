66cbe9ec0db0401131f4d20f7f9a497a48fce258
Jacek Obst <j.obst@teb-akademia.pl>
2021-01-24 16:57:44 +0100

    Remove redundant comment and model in CardObserver

--- app/Observers/CardObserver.php
+++ app/Observers/CardObserver.php
@@ -3,10 +3,8 @@
 namespace App\Observers;
 
 use App\Models\Card;
-use App\Models\User;
 use Carbon\Carbon;
 
-// Deleting the "-" sign from zip_code which will go to the database
 class CardObserver
 {
     public function creating(Card $card)
